002055cbaa7a2f63fc78f23a1517845b9329ff6f
RstoykoMadeit <rstoyko@hawk.iit.edu>
2024-11-13 01:29:15 -0600

    Map locations
    testing multiple map markers and a for loop

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+var positions = {
+	"6 Flags": { lat: 42.377740, lng:  -87.933897},
+	"Taco Bell": { lat: 42.049217, lng: -88.086170},
+	"IIT Tower": {lat: 41.833811, lng:  -87.626932}
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
-    center: { lat: 0, lng: 0},
+    center: positions["6 Flags"]
   });
-
-var marker = new google.maps.Marker({
-  position: { lat: 0, lng: 0},
-	map: map,
+for (var place in positions) {
+  var position = positions[place]; 
+  new google.maps.Marker({
+	  position: position,
+	  map: map,
+		title: place
 });
 }
