8edaed0cec29f7b6355a6e00c9ab69019cf83d20
RstoykoMadeit <rstoyko@hawk.iit.edu>
2024-11-13 01:31:23 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var positions = {
 	"6 Flags": { lat: 42.377740, lng:  -87.933897},
 	"Taco Bell": { lat: 42.049217, lng: -88.086170},
 	"IIT Tower": {lat: 41.833811, lng:  -87.626932}
-}
+};
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
