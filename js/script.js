7baebf5adb0f195f4bb1938009e7bb8ae65b8036
RstoykoMadeit <rstoyko@hawk.iit.edu>
2024-11-13 00:44:14 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
-    center: { "lat": 0, "lng": 0},
-  }
-);
+    center: { lat: 0, lng: 0},
+  });
+}
